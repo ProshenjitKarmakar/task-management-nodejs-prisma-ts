114259386f504e8436a44a795f70e6b6a7abe668
proshenjit <proshenjit@orangetoolz.com>
2024-09-20 12:33:46 +0600

    add build command

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "alive" });
+    res.json({ message: "alive here!" });
 });
 
 app.use('/api/v1', router)
