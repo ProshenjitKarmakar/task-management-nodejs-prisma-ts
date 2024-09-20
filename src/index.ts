b9068e715dd755b77838b757ba290ac8fface3a0
proshenjit <proshenjit@orangetoolz.com>
2024-09-20 12:58:55 +0600

    add domain to avoid cors error

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from 'express';
 import router from './routes/index.ts';
+import cors from 'cors';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(cors());
+
+app.use(cors({
+    origin: 'http://localhost:3000'  // Replace with your allowed domain
+}));
 app.get("/", (req, res) => {
     res.json({ message: "alive here!" });
 });
