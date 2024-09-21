3e14f00a5c0d4a6d75cc847379ad27de687f35ab
proshenjit <proshenjit@orangetoolz.com>
2024-09-21 11:07:47 +0600

    verify token added to scure apis

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import { TeskValidator } from './../validators/task.validator.ts';
 import { TaskController } from './../contollers/task.controller.ts';
+import varifyToken from './../helpers/verifyToken.ts';
 
 const router = Router();
 
 const taskValidator = new TeskValidator();
 const testController = new TaskController();
 
-router.get('/list', taskValidator.listTaskDataValidate, testController.listTask);
-router.post('/add', taskValidator.addTaskDataValidate, testController.addTask);
-router.put('/update', taskValidator.updateTaskDataValidate, testController.updateTask);
-router.delete('/delete', taskValidator.deleteTaskDataValidate, testController.deleteTask);
+router.get('/list', varifyToken, taskValidator.listTaskDataValidate, testController.listTask);
+router.post('/add', varifyToken, taskValidator.addTaskDataValidate, testController.addTask);
+router.put('/update', varifyToken, taskValidator.updateTaskDataValidate, testController.updateTask);
+router.delete('/delete', varifyToken, taskValidator.deleteTaskDataValidate, testController.deleteTask);
 
 export default router;
