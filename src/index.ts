c89bbe6cbe309154c718612960445e42a1d2b651
proshenjit <proshenjit@orangetoolz.com>
2024-09-21 10:14:40 +0600

    back to previous

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from 'express';
 import router from './routes/index.ts';
 import cors from 'cors';
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
-register('ts-node/esm', pathToFileURL('./'));
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -23,24 +21,14 @@ app.use(cors({
     }
 }));
 
-
 app.get("/", (req, res) => {
     res.json({ message: "alive here!" });
 });
 
 app.use('/api/v1', router)
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 }).on('error', (err) => {
     console.error('Error starting server:', err);
-});
-
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught Exception:', err);
-});
-
-process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
 });
